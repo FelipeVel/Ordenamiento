37b648a370bbca5f724a07e3e544ef419e280319
Felipe Velandia <fvelandia695@gmail.com>
2019-08-28 09:23:34 -0500

    Add files via upload

--- burbuja.py
+++ burbuja.py
@@ -5,19 +5,26 @@ Created on Tue Aug 27 20:41:24 2019
 @author: Felipe
 """
 
-import random
 
 def ordenar(lista):
+    global c
     for i in range (len(lista)-1):
-        for j in range(len(lista)):
-            if lista[j]>lista[i]:
+        c+=6
+        for j in range(i+1, n):
+            if lista[i]>lista[j]:
                 t=lista[i]
                 lista[i]=lista[j]
                 lista[j]=t
-                
+                c+=12
+    c+=2
+
+c=1             
 lista=[]
-for i in range(50):
-    lista.append(random.randint(1, 50))
+n=10
+for i in range(n):
+    lista.append(n-i)
 print(lista)
 ordenar(lista)
 print("Lista ordenada: "+str(lista))
+print("Numero de operaciones: "+str(c))
+print("Segun ecuacion: "+str((6*n*n) - 3))
